a04d97bd9bb62d15201d8cadd1fd3b24980d3507
Ivan Nieto <ivan.n.s@tuta.io>
2017-01-28 18:35:47 +0100

    Fix installation path generation for configuration file

--- t2u-driver-installer.py
+++ t2u-driver-installer.py
@@ -20,23 +20,25 @@ else:
     # Install files
     os.system("cd {} && make clean && make && sudo make install".format(INSTALL_FILES))
 
-    # Comprobar si existe el directorio de destino de la configuracion y si no existe crearlo
+    # Check for existing installation dirs
     print(os.path)
-    if os.path.isdir(DEV_DIR + '/Wireless/RT2870STA/'):
+    if os.path.isdir(PROD_DIR + '/Wireless'):
         pass
     else:
-        os.mkdir(DEV_DIR + '/Wireless/RT2870STA/')
+        os.mkdir(PROD_DIR + '/Wireless/RT2870STA/')
+        os.mkdir(PROD_DIR + '/Wireless/RT2870STA/')
+        
 
-    # Copiar el archivo de configuracion
-    os.system("sudo cp {}/RT2870STA.dat {}/Wireless/RT2870STA/RT2870STA.dat".format(INSTALL_FILES, DEV_DIR))
+    # Copy configuration file
+    os.system("sudo cp {}/RT2870STA.dat {}/Wireless/RT2870STA/RT2870STA.dat".format(INSTALL_FILES, PROD_DIR))
 
-    # Instalar el modulo del driver
+    # Install driver
     os.system("cd {}/os/linux/ && sudo insmod mt7650u_sta.ko".format(INSTALL_FILES))
 
-    # Generar el script que debería correrse al inicio del sistema para levantar la conexion
+    # Script generator for running t2u-driver as a system program
     os.system("sudo cp {0}/t2u-driver {1}/t2u-driver && sudo chmod +x {1}/t2u-driver".format(PATH, BIN_DIR))
 
-    # Avisar de las opciones para deshabilitar o levantar el adaptador desde el programa LOCAL_BIN
+    # Restart warning
     print("*"*25+"\n\nYour computer should be restarted now.\nPlease close all running programs and restart manually when you're done.\nAn executable will be installed to enable or disable the adapter.\nJust run in your terminal '$ sudo t2u-driver'.")    
 
 
