ebeb9281b4a486bae83b607973d65e04539dc030
Ivan Nieto <ivan.n.s@tuta.io>
2017-01-29 11:45:33 +0100

    Fix typos

--- t2u-driver-installer.py
+++ t2u-driver-installer.py
@@ -8,7 +8,6 @@ PROD_DIR = '/etc'
 BIN_DIR = '/usr/bin/'
 
 print(('*'*25)+'\n')
-print()
 
 def take_input():
     i = input("Please, disconnect all devices you're trying to install and press [I]: ")
@@ -27,7 +26,6 @@ else:
     else:
         os.mkdir(PROD_DIR + '/Wireless/')
         os.mkdir(PROD_DIR + '/Wireless/RT2870STA/')
-        
 
     # Copy configuration file
     os.system("sudo cp {}/RT2870STA.dat {}/Wireless/RT2870STA/RT2870STA.dat".format(INSTALL_FILES, PROD_DIR))
@@ -36,7 +34,7 @@ else:
     os.system("cd {}/os/linux/ && sudo insmod mt7650u_sta.ko".format(INSTALL_FILES))
 
     # Script generator for running t2u-driver as a system program
-    os.system("sudo cp {0}/t2u-driver {1}/t2u-driver && sudo chmod +x {1}/t2u-driver".format(PATH, BIN_DIR))
+    os.system("sudo cp {}/t2u-driver {}/t2u-driver && sudo chmod +x {1}/t2u-driver".format(PATH, BIN_DIR))
 
     # Restart warning
     print("*"*25+"\n\nYour computer should be restarted now.\nPlease close all running programs and restart manually when you're done.\nAn executable will be installed to enable or disable the adapter.\nJust run in your terminal '$ sudo t2u-driver'.")    
